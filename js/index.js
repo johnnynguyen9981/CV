52546bbd30f79e127e5d6d9b676e056721047577
johnnynguyen9981 <87358321+johnnynguyen9981@users.noreply.github.com>
2021-08-17 23:34:48 +0700

    ver12++

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
-$("img.logo").on("hover", function(){
-  $(this).addClass(".logoClicked");
+
+
+//************* Home Page *******//
+
+//Hover Zoom Logo
+$("img.logo").hover(function(){
+  $(this).addClass("logoClicked");
+}, function(){
+  $(this).removeClass("logoClicked");
+});
+
+//Hover Zoom IconicWhite
+$("img.iconicWhite").hover(function(){
+  $(this).addClass("iconicWhiteClicked");
+}, function(){
+  $(this).removeClass("iconicWhiteClicked");
+});
+
+
+//************* Contact Page *******//
+
+//Hover Zooming the BC_Back
+$("img.BC_Back").hover(function(){
+  $(this).addClass("BC_Back_Hover");
+},function(){
+  $(this).removeClass("BC_Back_Hover");
+});
+
+//Hover zooming the BC_Front
+$("img.BC_Front").hover(function(){
+  $(this).addClass("BC_Front_Hover");
+}, function(){
+  $(this).removeClass("BC_Front_Hover");
 });
