ead4ae7b83464c3f6505d4f1459c3a8d47916b5a
johnnynguyen9981 <87358321+johnnynguyen9981@users.noreply.github.com>
2021-09-12 23:06:14 +0700

    ver21

--- js/index.js
+++ js/index.js
@@ -1,22 +1,66 @@
 // Home Page
 
 // Logo Hover Effect
-$("img.logo").hover(function(){
+$("img.logo").hover(function() {
   $(this).addClass("logoHover");
-},function(){
+}, function() {
   $(this).removeClass("logoHover");
 });
 
 // IconicWhite Hover Effect
-$("img.iconicWhite").hover(function(){
+$("img.iconicWhite").hover(function() {
   $(this).addClass("iconicWhiteHover");
-}, function(){
+}, function() {
   $(this).removeClass("iconicWhiteHover");
 });
 
 // Avatar Hover Effect
-$("img.avatar").hover(function(){
+$("img.avatar").hover(function() {
   $(this).addClass("avatarHover");
-}, function(){
+}, function() {
   $(this).removeClass("avatarHover");
+});
+
+// Movement for the animation to happen
+
+
+//Moving Animation Event
+
+const card = document.querySelector(".aCard");
+const container = document.querySelector(".aContainer");
+
+// items
+
+const title = document.querySelector(".aTitle");
+const logo = document.querySelector(".aLogo");
+const subtitle = document.querySelector(".aSubtitle");
+const button = document.querySelector(".aButton");
+
+// Moving element event
+container.addEventListener("mousemove", (e) =>{
+  let yAxis = (window.innerWidth/2  - e.pageY) / 30;
+  let xAxis = (window.innerHeight/2  - e.pageX) / 30;
+  card.style.transform = `rotateX(${-yAxis}deg) rotateY(${-xAxis}deg) `;
+});
+
+//Animate in
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+
+  // popout
+  logo.style.transform = "translateZ(100px)";
+  title.style.transform = "translateZ(100px)";
+  subtitle.style.transform = "translateZ(100px)";
+});
+
+
+
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+    card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+
+  // popback
+  title.style.transform = "translateZ(0px)";
+  logo.style.transform = "translateZ(0px)";
 });
